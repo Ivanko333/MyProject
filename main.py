d42291dc549dd0dc3c81a69c2bc8c55949af463f
Your a <a@example.com>
2023-12-19 20:14:42 +0300

    edited

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Win(QWidget):
         super().__init__()
         self.set()
         self.initUI()
-        # self.connects()
+        self.connects()
 
     def set(self):
         self.setWindowTitle("Конвертатор")
@@ -16,24 +16,42 @@ class Win(QWidget):
     def initUI(self):
         text1 = QLabel("Введите температуру в градусах Цельсия:")
         self.input1 = QLineEdit("")
-        self.button = QPushButton("Отправить")
+        self.button1 = QPushButton("Отправить")
+        self.button2 = QPushButton("Отправить")
         text2 = QLabel("В градусах Фаренгейта:")
-        input2 = QLineEdit("")
+        self.input2 = QLineEdit()
         line = QVBoxLayout()
-        line.setSpacing(10)
+        line1 = QVBoxLayout()
+        line2 = QHBoxLayout()
         line.addWidget(text1, alignment=Qt.AlignLeft)
         line.addWidget(self.input1, alignment=Qt.AlignLeft)
-        line.addWidget(self.button, alignment=Qt.AlignLeft)
-        line.addWidget(text2, alignment=Qt.AlignLeft)
-        line.addWidget(input2, alignment=Qt.AlignLeft)
-        self.setLayout(line)
+        line.addWidget(self.button1, alignment=Qt.AlignLeft)
+        line1.addWidget(text2, alignment=Qt.AlignLeft)
+        line1.addWidget(self.input2, alignment=Qt.AlignLeft)
+        line1.addWidget(self.button2, alignment=Qt.AlignLeft)
+        line2.addLayout(line)
+        line2.addLayout(line1)
+        self.setLayout(line2)
 
-    def event(self):
-        a = self.input1.text()
-        print(a)
+    def event1(self):
+        try:
+            a = float(self.input1.text())
+            b = a * 1.8 + 32
+            c = self.input2.setText(f"{b}")
+        except:
+            self.input1.setText("Введите ЦЕЛОЕ число")
 
-    # def connects(self):
-    #     self.button.clicked.connect(event)
+    def event2(self):
+        try:
+            a = float(self.input2.text())
+            b = (a - 32) / 1.8
+            с = self.input1.setText(f"{b}")
+        except:
+            self.input2.setText("Введите ЦЕЛОЕ число")
+
+    def connects(self):
+        self.button1.clicked.connect(self.event1)
+        self.button2.clicked.connect(self.event2)
 
 
 app = QApplication([])
